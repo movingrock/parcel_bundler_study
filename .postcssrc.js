ab4c1fb850635adcb42bca6d40dfac97f3bb2c6f
movingrock <75603892+movingrock@users.noreply.github.com>
2024-01-03 17:27:34 +0900

    Create Project

---
+++ .postcssrc.js
@@ -0,0 +1,18 @@
+// ESM 에크마스크립트 모듈
+// import 
+// export 
+
+
+
+// CommonJS
+// nodejs 환경에서는 import 대신 require() 사용
+// export  대신에 module.exports 사용
+// require()
+// module.exports
+
+const autoprefixer = require('autoprefixer')
+module.exports = {
+  plugins: [
+    autoprefixer
+  ]
+}
